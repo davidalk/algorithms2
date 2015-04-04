43f2d69979598f746641cccf5516cc9f7ba0e8c6
davidalk <david.alkanani@gmail.com>
2015-04-04 15:09:37 +0100

    week2

---
+++ week2/src/main/java/uk/co/alkanani/clustering/ClusteringAlgorithm.java
@@ -0,0 +1,70 @@
+package uk.co.alkanani.clustering;
+
+import uk.co.alkanani.domain.Edge;
+import uk.co.alkanani.domain.Graph;
+import uk.co.alkanani.domain.Node;
+
+import java.util.*;
+
+public class ClusteringAlgorithm {
+    private final Graph graph;
+
+    public ClusteringAlgorithm(Graph graph) {
+        this.graph = graph;
+    }
+
+    public Set<Edge> execute(int targetClusters) {
+        Node[] nodes = initialiseNodes();
+        int clusterCount = graph.nodeCount;
+        List<Edge> sortedEdges = getSortedEdges();
+
+
+        Iterator<Edge> edgeIterator = sortedEdges.iterator();
+
+        while (clusterCount > targetClusters) {
+            Edge edge = edgeIterator.next();
+            if (nodes[edge.a].findSet() != nodes[edge.b].findSet()) {
+                nodes[edge.a].union(nodes[edge.b]);
+                clusterCount--;
+            }
+            edgeIterator.remove();
+        }
+
+        Collections.reverse(sortedEdges);
+        Set<Edge> resultSet = new HashSet<>();
+        Iterator<Edge> reverseEdgeIterator = sortedEdges.iterator();
+        while (resultSet.size() < targetClusters - 1) {
+            Edge edge = reverseEdgeIterator.next();
+            if (nodes[edge.a].findSet() != nodes[edge.b].findSet()) {
+                resultSet.add(edge);
+            }
+        }
+        return resultSet;
+    }
+
+    private Node[] initialiseNodes() {
+        Node[] nodes = new Node[graph.nodeCount+1];
+
+        for (int i=1; i<=graph.nodeCount; i++) {
+            nodes[i] = new Node(i);
+        }
+        return nodes;
+    }
+
+    List<Edge> getSortedEdges() {
+        List<Edge> edges = graph.edges;
+
+        Collections.sort(edges, (e1, e2) -> {
+            long diff = e1.weight - e2.weight;
+            if (diff == 0) {
+                return 0;
+            } else if (diff > 0) {
+                return 1;
+            } else {
+                return -1;
+            }
+        });
+
+        return edges;
+    }
+}
---
+++ week2/src/test/java/uk/co/alkanani/clustering/ClusteringAlgorithmTest.java
@@ -0,0 +1,57 @@
+package uk.co.alkanani.clustering;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import uk.co.alkanani.domain.Edge;
+import uk.co.alkanani.domain.Graph;
+import uk.co.alkanani.file.FileUtil;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import static org.junit.Assert.*;
+
+@RunWith(JUnit4.class)
+public class ClusteringAlgorithmTest {
+
+    @Test
+    public void clusteringAlgorithmGeneratesCorrectMaximumEdgeSet() {
+        // given
+        Edge edge1 = new Edge(5, 6, 10);
+        Edge edge2 = new Edge(2, 8, 11);
+        Set<Edge> expected = new HashSet<>();
+        expected.add(edge1);
+        expected.add(edge2);
+        Graph graph = FileUtil.loadEdges("clustering-test.txt");
+
+        // when
+        ClusteringAlgorithm algorithm = new ClusteringAlgorithm(graph);
+        Set<Edge> result = algorithm.execute(3);
+
+        // then
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void edgeSortMethodReturnsCorrectList() {
+        // given
+        Edge edge1 = new Edge(1, 2, 10);
+        Edge edge2 = new Edge(2, 3, 2);
+        Edge edge3 = new Edge(3, 4, 15);
+        Edge edge4 = new Edge(4, 5, 7);
+        List<Edge> edges = Arrays.asList(edge1, edge2, edge3, edge4);
+        Graph graph = new Graph(5, edges);
+
+        List<Edge> expected = Arrays.asList(edge2, edge4, edge1, edge3);
+
+        // when
+        ClusteringAlgorithm algorithm = new ClusteringAlgorithm(graph);
+        List<Edge> result = algorithm.getSortedEdges();
+
+        // then
+        assertEquals(expected, result);
+    }
+}
