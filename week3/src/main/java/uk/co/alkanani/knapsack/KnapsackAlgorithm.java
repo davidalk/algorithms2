d6454634fcfac846ad04a11a7fd18a3eeac81f63
davidalk <david.alkanani@gmail.com>
2015-04-12 14:13:51 +0100

    week3

--- week3/src/main/java/uk/co/alkanani/knapsack/KnapsackAlgorithm.java
+++ week3/src/main/java/uk/co/alkanani/knapsack/KnapsackAlgorithm.java
@@ -1,5 +1,6 @@
 package uk.co.alkanani.knapsack;
 
+import uk.co.alkanani.domain.Item;
 import uk.co.alkanani.domain.Knapsack;
 
 public class KnapsackAlgorithm {
@@ -11,6 +12,30 @@ public class KnapsackAlgorithm {
 
     public long calculateOptimalValue() {
         long[][] table = new long[knapsack.itemCount + 1][knapsack.size + 1];
-        return 0;
+        Item[] items = knapsack.items;
+
+        for (int x=0; x<=knapsack.size; x++) {
+            table[0][x] = 0;
+        }
+
+        for (int i=1; i<=knapsack.itemCount; i++) {
+            for (int x=0; x<=knapsack.size; x++) {
+                long val1 = table[i-1][x];
+                long val2 = 0;
+
+                int offset = x - items[1].weight;
+                if (offset >= 0) {
+                    val2 = table[i-1][offset] + items[i].value;
+                }
+
+                if (val1 > val2) {
+                    table[i][x] = val1;
+                } else {
+                    table[i][x] = val2;
+                }
+            }
+        }
+
+        return table[knapsack.itemCount][knapsack.size];
     }
 }
