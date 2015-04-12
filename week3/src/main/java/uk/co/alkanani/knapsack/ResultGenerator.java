543f25135bd0fb32459850b5cda04f77a41535b2
davidalk <david.alkanani@gmail.com>
2015-04-12 14:36:31 +0100

    week3

---
+++ week3/src/main/java/uk/co/alkanani/knapsack/ResultGenerator.java
@@ -0,0 +1,20 @@
+package uk.co.alkanani.knapsack;
+
+import uk.co.alkanani.domain.Knapsack;
+import uk.co.alkanani.file.FileUtil;
+
+public class ResultGenerator {
+
+    public static void main(String... args) {
+        Knapsack smallKnapsack = FileUtil.loadKnapsack("knapsack1.txt");
+        KnapsackAlgorithm algorithm = new KnapsackAlgorithm(smallKnapsack);
+        long optimalSmall = algorithm.calculateOptimalValue();
+        System.out.println("Optimal small value " + optimalSmall);
+
+        Knapsack largeKnapsack = FileUtil.loadKnapsack("knapsack_big.txt");
+        KnapsackAlgorithm largeAlgorithm = new KnapsackAlgorithm(largeKnapsack);
+        long optimalLarge = largeAlgorithm.calculateOptimalValue();
+        System.out.println("Optimal small value " + optimalLarge);
+
+    }
+}
