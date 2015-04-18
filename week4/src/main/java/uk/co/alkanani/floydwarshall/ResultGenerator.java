190f113f7a70c258633d25d639b69bfa247e20d9
davidalk <david.alkanani@gmail.com>
2015-04-18 15:31:56 +0100

    week4

--- week4/src/main/java/uk/co/alkanani/floydwarshall/ResultGenerator.java
+++ week4/src/main/java/uk/co/alkanani/floydwarshall/ResultGenerator.java
@@ -15,6 +15,14 @@ public class ResultGenerator {
 
         Graph graph2 = FileUtil.loadGraph("g2.txt");
         FloydWarshallAlgorithm algorithm2 = new FloydWarshallAlgorithm(graph2);
+        FloydWarshallResult result2 = algorithm2.execute();
 
+        System.out.println("Graph 2, shortest path: " + result2.shortestPathLength + " negative cycle: " + result2.containsNegativeCycle);
+
+        Graph graph3 = FileUtil.loadGraph("g3.txt");
+        FloydWarshallAlgorithm algorithm3 = new FloydWarshallAlgorithm(graph3);
+        FloydWarshallResult result3 = algorithm3.execute();
+
+        System.out.println("Graph 3, shortest path: " + result3.shortestPathLength + " negative cycle: " + result3.containsNegativeCycle);
     }
 }
