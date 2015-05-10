9141ebcdbb2c46909560547c415f975ec2f98aef
davidalk <david.alkanani@gmail.com>
2015-05-10 12:49:03 +0100

    week6

---
+++ week6/src/test/java/uk/co/alkanani/domain/TwoSatClauseTest.java
@@ -0,0 +1,27 @@
+package uk.co.alkanani.domain;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import static org.junit.Assert.*;
+
+@RunWith(JUnit4.class)
+public class TwoSatClauseTest {
+
+    @Test
+    public void isSatisfiedReturnsCorrectResult() {
+        // given
+        TwoSatClause clause = new TwoSatClause(true, 3, false, 1);
+        boolean[] arg1 = {false, true, true, true};
+        boolean[] arg2 = {false, false, true, false};
+        boolean[] arg3 = {false, true, false, false};
+        boolean[] arg4 = {true, false, false, true};
+
+        // when & then
+        assertTrue(clause.isSatisfied(arg1));
+        assertTrue(clause.isSatisfied(arg2));
+        assertTrue(clause.isSatisfied(arg3));
+        assertFalse(clause.isSatisfied(arg4));
+    }
+}
